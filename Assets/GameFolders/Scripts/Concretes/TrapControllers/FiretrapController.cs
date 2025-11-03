221ebd7869680ae5a4a42db4b89b557d2608cd24
ArpanenduKhag <arpanendu2403@gmail.com>
2025-11-03 17:59:10 +0530

    Web BUILD AND commented code

--- Assets/GameFolders/Scripts/Concretes/TrapControllers/FiretrapController.cs
+++ Assets/GameFolders/Scripts/Concretes/TrapControllers/FiretrapController.cs
@@ -3,57 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using Abstracts;
 using Combat;
+
 namespace Controllers
 {
+
     public class FiretrapController : Traps
     {
-        Animator _anim;
-        BoxCollider2D _collider;
-        float _currentTime;
+        Animator _anim;           
+        BoxCollider2D _collider;   
+        float _currentTime;        
 
-        [SerializeField] float _maxTime;
-        [SerializeField] float _startDelay;
+        [SerializeField] float _maxTime;     
+        [SerializeField] float _startDelay;  
         private void Awake()
         {
+            // Get required components and initialize trap state.
             _collider = GetComponent<BoxCollider2D>();
-            _collider.enabled = false;
-            _anim = GetComponent<Animator>();
+            _collider.enabled = false;        
+            _anim = GetComponent<Animator>();  
+
             _hitDamage = GetComponent<Damage>();
         }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            // When something enters the firetrap's collider, deal damage.
             HitTarget(collision);
-
         }
+
         private void OnTriggerStay2D(Collider2D collision)
         {
+            // While the target stays in the firetrap, apply additional effects like knockback or bounce.
             MakeTargetJump(collision);
         }
+
         private void Update()
         {
-
+            // Wait until the initial delay period has passed before starting the trap logic.
             if (Time.timeSinceLevelLoad < _startDelay) return;
+
+            // Increment timer by time passed per frame.
             _currentTime += Time.deltaTime;
+
+            // FIRE ACTIVE 
             if (_currentTime > _maxTime)
             {
-                _anim.SetBool("IsFire", true);
-                _anim.SetBool("PreFire", false);
-                _collider.enabled = true;
-                _currentTime = 0f;
+                _anim.SetBool("IsFire", true);     // Trigger fire animation.
+                _anim.SetBool("PreFire", false);   // Disable pre-fire animation.
+                _collider.enabled = true;          // Enable collider (trap can deal damage now).
+                _currentTime = 0f;                 // Reset timer for next cycle.
             }
+
+            // PRE-FIRE WARNING  
             else if (_currentTime > _maxTime / 1.18)
             {
-                _anim.SetBool("PreFire", true);
+                _anim.SetBool("PreFire", true);    // Trigger pre-fire animation (warning visuals).
             }
+
+            // FIRE COOLDOWN  
             else if (_currentTime > _maxTime / 3)
             {
-                _anim.SetBool("IsFire", false);
-                _collider.enabled = false;
+                _anim.SetBool("IsFire", false);    // Stop fire animation.
+                _collider.enabled = false;         // Disable collider (no damage now).
             }
-
-
         }
-
     }
-
 }
--- Assets/GameFolders/Scripts/Concretes/TrapControllers/SawController.cs
+++ Assets/GameFolders/Scripts/Concretes/TrapControllers/SawController.cs
@@ -7,62 +7,77 @@ using Combat;
 
 public class SawController : Traps
 {
-    [SerializeField] Transform[] _patrolPoints;
-    [SerializeField] float _moveSpeed;
-    [SerializeField] Transform SawPositionsParent;
-    Vector2[] _patrolPositions;
-    Vector2 _movePos;
-    int _patrolPointIndex = 0;
+    [SerializeField] Transform[] _patrolPoints;         
+    [SerializeField] float _moveSpeed;            
+    [SerializeField] Transform SawPositionsParent;  
+
+    Vector2[] _patrolPositions;                     
+    Vector2 _movePos;                                
+    int _patrolPointIndex = 0;                       
     Vector3 _currentPointPos;
-    Vector3 _startPointPos;
+    Vector3 _startPointPos;                          
     
     private void Awake()
     {
+        // Inherit the _hitDamage functionality from the Traps class
+        // and get the Damage component attached to this saw.
         _hitDamage = GetComponent<Damage>();
     }
+
     private void Start()
     {
+        // Initialize first patrol point and record starting position.
         _currentPointPos = _patrolPoints[0].position;
         _startPointPos = transform.position;
-        _patrolPositions= new Vector2[_patrolPoints.Length];
-        foreach(Transform position in _patrolPoints)
+
+        // Store patrol points and organize them under the parent for clarity.
+        _patrolPositions = new Vector2[_patrolPoints.Length];
+        foreach (Transform position in _patrolPoints)
         {
             position.SetParent(SawPositionsParent);
         }
-        
     }
+
     private void Update()
     {
+        // Move toward the current patrol point.
         _movePos = _currentPointPos - transform.position;
         transform.Translate(_movePos.normalized * _moveSpeed * Time.deltaTime, Space.World);
-        if(Vector3.Distance(transform.position,_currentPointPos)<= 1f)
+
+        // If the saw is close enough to the current patrol point, go to the next one.
+        if (Vector3.Distance(transform.position, _currentPointPos) <= 1f)
         {
             NextPoint();
         }
-        
     }
 
     private void NextPoint()
     {
         _patrolPointIndex++;
-        if(_patrolPointIndex == _patrolPoints.Length)
-        { 
-            _currentPointPos = _startPointPos;
-            return; 
-        }
-        else if(_patrolPointIndex > _patrolPoints.Length)
+
+        if (_patrolPointIndex == _patrolPoints.Length)
         {
+            // Return to start point once all patrol points are covered.
+            _currentPointPos = _startPointPos;
+            return;
+        }
+        else if (_patrolPointIndex > _patrolPoints.Length)
+        {
+            // Reset index when reaching the end of patrol list.
             _patrolPointIndex = 0;
         }
+
+        // Set the next target patrol point.
         _currentPointPos = _patrolPoints[_patrolPointIndex].position;
     }
 
+    // Detect player collisions to apply damage and knockback.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            HitTarget(collision);
-            MakeTargetJump(collision);
+            HitTarget(collision);       
+            MakeTargetJump(collision);  
         }
     }
 }
